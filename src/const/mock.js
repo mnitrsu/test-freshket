e5100eb233d933157e9baeae4fa1c615a86050da
man <mnitrsu@gmail.com>
2024-11-13 11:51:39 +0700

    [build] mock constant

---
+++ src/const/mock.js
@@ -0,0 +1,58 @@
+export const menus = [
+  {
+    id: "c8392503-24e0-4401-9cf5-c1d8c24039b4",
+    name: "Red set",
+    price: 50,
+  },
+  {
+    id: "dd4f40f2-4c9a-4d2e-8b52-d09e7ca25613",
+    name: "Green set",
+    price: 40,
+  },
+  {
+    id: "dfffeffa-7844-456b-a5d3-dcef6fdeab9b",
+    name: "Blue set",
+    price: 30,
+  },
+  {
+    id: "6b2f057e-ae5a-4dae-bbca-7a5c41606cc4",
+    name: "Yellow set",
+    price: 50,
+  },
+  {
+    id: "10443eb5-3207-483d-a67f-4fae9a286a5a",
+    name: "Pink set",
+    price: 80,
+  },
+  {
+    id: "e280fd8b-6263-4b59-8370-97c3db9e11fd",
+    name: "Purple set",
+    price: 90,
+  },
+  {
+    id: "689939f7-1518-459f-9603-51a94f45044d",
+    name: "Orange set",
+    price: 120,
+  },
+];
+
+export const bundles = [
+  {
+    id: "009df691-434d-491c-aff1-285ea0ba07fe",
+    bundle_of: "689939f7-1518-459f-9603-51a94f45044d",
+    amount: 2,
+    discount: 0.05,
+  },
+  {
+    id: "fcbbb23d-41f8-44de-a015-c8a3b874c648",
+    bundle_of: "10443eb5-3207-483d-a67f-4fae9a286a5a",
+    amount: 2,
+    discount: 0.05,
+  },
+  {
+    id: "c8496291-860e-4d70-ad6e-cd77c38f5cef",
+    bundle_of: "dd4f40f2-4c9a-4d2e-8b52-d09e7ca25613",
+    amount: 2,
+    discount: 0.05,
+  },
+];
